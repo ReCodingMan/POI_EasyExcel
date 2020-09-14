78e4d3894a44d6f3a307adea21c5acfed35e077e
chengcheng1021 <chengcheng1021@aliyun.com>
2020-09-14 20:54:12 +0800

    读取不同类型数据

--- kunag-poi/src/main/java/com/cc/ExcelReadTest.java
+++ kunag-poi/src/main/java/com/cc/ExcelReadTest.java
@@ -62,4 +62,46 @@ public class ExcelReadTest {
         System.out.println(cell.getNumericCellValue());
         fileInputStream.close();
     }
+
+    @Test
+    public void testCellType() throws IOException{
+        //获取文件流
+        FileInputStream fileInputStream = new FileInputStream(PATH + "更新公式.xls");
+
+        //1、创建一个工作簿 03，使用Excel能操作的这边都可以操作！
+        Workbook workbook = new HSSFWorkbook(fileInputStream);
+
+        //获取标题内容
+        Sheet sheet = workbook.getSheetAt(0);
+        Row rowTitle = sheet.getRow(0);
+        if (rowTitle != null) {
+            // 一定要掌握
+            int cellCount = rowTitle.getPhysicalNumberOfCells();
+            for (int cellNum = 0; cellNum < cellCount; cellNum++) {
+                Cell cell = rowTitle.getCell(cellNum);
+                if (cell != null) {
+                    int cellType = cell.getCellType();
+                    String cellValue = cell.getStringCellValue();
+                    System.out.print(cellValue + " | ");
+                }
+            }
+            System.out.println();
+        }
+
+        // 获取表中内容
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        for (int rowNum = 0; rowNum < rowCount; rowNum++) {
+            Row rowData = sheet.getRow(rowNum);
+            if (rowData != null) {
+                //读取列
+                int cellCount = rowTitle.getPhysicalNumberOfCells();
+                for (int cellNum = 0; cellNum < cellCount; cellNum++) {
+                    System.out.print("[" + (rowNum+1) + "-" + (cellNum+1) + "]");
+
+                    Cell cell = rowData.getCell(cellNum);
+                    //匹配列的数据类型
+                }
+            }
+        }
+    }
 }
